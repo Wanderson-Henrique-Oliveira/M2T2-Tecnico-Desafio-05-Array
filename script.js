1cb2d8c5e8776b47dedc0d0d419ffc37afe122ad
Henrique Oliveira <wandholiveira1@gmail.com>
2022-06-27 16:44:01 -0300

    Estrutura de dados: Array

---
+++ script.js
@@ -0,0 +1,31 @@
+//Array padrão: Exibindo no console.
+let strangerThingsKids = ['Mike', 'Will', 'Dustin', 'Lucas', 'Eleven'];
+
+console.log(strangerThingsKids);
+
+//Array: Unshift.
+let addNewKid = strangerThingsKids.unshift('Max');
+
+console.log(strangerThingsKids);
+
+//Array: Pop.
+let removeLastKid = strangerThingsKids.pop();
+
+console.log(strangerThingsKids);
+
+//Array: Push.
+let addMoreKids = strangerThingsKids.push('Eleven', 'Susie');
+
+console.log(strangerThingsKids);
+
+//Array: Shift.
+let removeFirstKid = strangerThingsKids.shift('Max');
+
+console.log(strangerThingsKids);
+
+//Array: Sort.
+let numbers = [7,5,6,3,8,9,2,1,4];
+
+numbers.sort();
+
+console.log(numbers);
